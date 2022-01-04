3e503453444bebad1e9656abec88669776658424
rsbettini <robson.bettini@hotmail.com>
2022-01-04 20:46:53 -0300

    Backend project created

---
+++ backend/src/main/java/com/rsbettini/shopcatalog/ShopcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.rsbettini.shopcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rsbettini/shopcatalog/ShopcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsbettini.shopcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
