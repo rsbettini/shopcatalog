0524ca3e893da95fd71145c2923e78d6f6423949
rsbettini <robson.bettini@hotmail.com>
2022-04-14 16:57:20 -0300

    Admin Navbar layuot mobile

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,5 +1,16 @@
+import Navbar from "./Navbar";
+
+import "./styles.css";
+
 const Admin = () => {
-  return <div>Página Admin</div>;
+  return (
+    <div className="admin-container">
+      <Navbar />
+      <div className="admin-content">
+        <h1>Conteúdo</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
