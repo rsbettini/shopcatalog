2c6cfd794c821cf272857fa0302647c5bdfe56c5
rsbettini <robson.bettini@hotmail.com>
2022-04-29 21:00:15 -0300

    PrivateRoute

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,25 +1,25 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 import Users from "./User";
+import PrivateRoute from "components/PrivateRoute";
 
 import "./styles.css";
-
 
 const Admin = () => {
   return (
     <div className="admin-container">
       <Navbar />
       <div className="admin-content">
-        ,<Switch>
-          <Route path="/admin/products">
+        <Switch>
+          <PrivateRoute path="/admin/products">
             <h1>Product CRUD</h1>
-          </Route>
-          <Route path="/admin/categories">
+          </PrivateRoute>
+          <PrivateRoute path="/admin/categories">
             <h1>Category CRUD</h1>
-          </Route>
-          <Route path="/admin/users">
+          </PrivateRoute>
+          <PrivateRoute path="/admin/users">
             <Users />
-          </Route>
+          </PrivateRoute>
         </Switch>
       </div>
     </div>
