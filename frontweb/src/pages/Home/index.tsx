f822244d0a786414e3408f39c0c3d4e8a38cbc5d
rsbettini <robson.bettini@hotmail.com>
2022-04-30 21:14:07 -0300

    Verification hasAnyRoles

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
 import { ReactComponent as MainImage } from "assets/images/main-image.svg";
 import ButtonIcon from "components/ButtonIcon";
 import { Link } from "react-router-dom";
-import { isAuthenticated } from "util/requests";
+import { hasAnyRoles, isAuthenticated } from "util/requests";
 
 import "./styles.css";
 
 const Home = () => {
   return (
     <div className="home-container">
+      <h1>Resultado = {hasAnyRoles(['ROLE_ADMIN', 'ROLE_OPERATOR']) ? 'sim' : 'não'}</h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -116,3 +116,27 @@ export const isAuthenticated = () : boolean => {
   const tokenData = getTokenData();
   return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 }
+
+export const hasAnyRoles = (roles: Role[]) : boolean => {
+
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+
+  /* Form mais exuta de impekentar a verificação
+  if (tokenData !== undefined) {
+    return roles.some(role => tokenData.authorities.includes(role));
+  }*/
+  
+  if (tokenData !== undefined) {
+    for (var i=0; i < roles.length; i++){
+      if (tokenData.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
