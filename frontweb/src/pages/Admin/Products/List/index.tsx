5366283ae5e0277adb19b23acb409d2a0505b9ef
rsbettini <robson.bettini@hotmail.com>
2022-05-31 18:35:35 -0300

    Product listing from back end

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,29 +1,30 @@
+import { AxiosRequestConfig } from "axios";
 import ProductCrudCard from "pages/Admin/Products/ProductCrudCard";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Product } from "types/product";
+import { SpringPage } from "types/vendor/spring";
+import { requestBackend } from "util/requests";
 
 import "./styles.css";
 
 const List = () => {
-  const product = {
-    id: 5,
-    name: "Rails for Dummies",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    price: 100.99,
-    imgUrl:
-      "https://raw.githubusercontent.com/rsbettini/shopcatalog/main/backend/img/5-big.jpg",
-    date: "2020-07-14T10:00:00Z",
-    categories: [
-      {
-        id: 2,
-        name: "Electronics",
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: "GET",
+      url: "/products",
+      params: {
+        page: 0,
+        size: 50,
       },
-      {
-        id: 1,
-        name: "Books",
-      },
-    ],
-  };
+    };
+
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
   return (
     <div className="product-grud-container">
@@ -36,18 +37,11 @@ const List = () => {
         <div className="base-card product-filter-container">Search bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map((product) => (
+          <div key={product.id} className="col-sm-6 col-md-12">
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
     </div>
   );
