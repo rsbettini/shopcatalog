368f8d86090b0fa04a45a0c756e475e8ee830ff4
rsbettini <robson.bettini@hotmail.com>
2022-06-01 19:21:52 -0300

    Retrieving categories from back end

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,8 +1,9 @@
 import { AxiosRequestConfig } from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useHistory, useParams } from "react-router-dom";
 import Select from "react-select";
+import { Category } from "types/category";
 import { Product } from "types/product";
 import { requestBackend } from "util/requests";
 
@@ -13,17 +14,14 @@ type UrlParams = {
 };
 
 const Form = () => {
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
-
+  
   const { productId } = useParams<UrlParams>();
 
   const isEditing = productId !== "create";
 
   const history = useHistory();
+
+  const [selectCategories, setSelectCategories] = useState<Category[]>([]);
 
   const {
     register,
@@ -31,6 +29,13 @@ const Form = () => {
     formState: { errors },
     setValue,
   } = useForm<Product>();
+
+  useEffect(() => {
+    requestBackend({url: '/categories'})
+    .then(response => {
+      setSelectCategories(response.data.content);
+    })
+  }, []);
 
   useEffect(() => {
     if (isEditing) {
@@ -96,24 +101,15 @@ const Form = () => {
                 </div>
               </div>
 
-
-
-
-
-
-
               <div className="margin-button-30">
                 <Select 
-                  options={options}
+                  options={selectCategories}
                   classNamePrefix="product-crud-select"
                   isMulti
+                  getOptionLabel={(category: Category) => category.name}
+                  getOptionValue={(category: Category) => String(category.id)}
                 />
               </div>
-
-
-
-
-
 
               <div className="margin-button-30">
                 <input
