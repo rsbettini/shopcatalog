1afc83fecdcd50ecb433518593c4dc42da1c0ee2
Robson Bettini <robson.bettini@hotmail.com>
2022-06-25 22:04:47 -0300

    ButtonIcon test

---
+++ frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import ButtonIcon from "..";
+import "@testing-library/jest-dom/extend-expect";
+
+test("ButtonIcon should render button with given text", () => {
+  const text = "Fazer login";
+
+  render(<ButtonIcon text={text} />);
+
+  expect(screen.getByText(text)).toBeInTheDocument();
+});
