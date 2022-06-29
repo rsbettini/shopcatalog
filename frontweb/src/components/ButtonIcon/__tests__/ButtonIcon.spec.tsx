b8d2a615a1af013dfce58677a91a206b4d186ac9
Robson Bettini <robson.bettini@hotmail.com>
2022-06-29 20:54:10 -0300

    Data-testid demonstration

--- frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
+++ frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -8,4 +8,5 @@ test("ButtonIcon should render button with given text", () => {
   render(<ButtonIcon text={text} />);
 
   expect(screen.getByText(text)).toBeInTheDocument();
+  expect(screen.getByTestId("arrow")).toBeInTheDocument();
 });
