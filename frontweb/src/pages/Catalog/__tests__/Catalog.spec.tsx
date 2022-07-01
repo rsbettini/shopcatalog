a50aaab99b54c249da33b57d5365679c8f2a64cd
Robson Bettini <robson.bettini@hotmail.com>
2022-07-01 14:38:48 -0300

    MSW mock server, fixtures

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -3,20 +3,22 @@ import Catalog from "..";
 import "@testing-library/jest-dom/extend-expect";
 import { Router } from "react-router-dom";
 import history from "util/history";
+import { server } from "./fixtures";
 
-test("ButtonIcon should render Catalog with products", async () => {
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("should render Catalog with products", async () => {
   render(
     <Router history={history}>
       <Catalog />
     </Router>
   );
 
-  //screen.debug();
-
   expect(screen.getByText("Catálogo de Produtos")).toBeInTheDocument();
 
   await waitFor(() => {
     expect(screen.getByText("Smart TV")).toBeInTheDocument();
   });
-  //screen.debug();
 });
